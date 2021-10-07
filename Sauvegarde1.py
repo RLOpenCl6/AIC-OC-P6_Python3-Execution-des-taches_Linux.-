1f6f4542e66c9c8fe23986d35758dab4bc458bbe
RLOpenCl6 <90632871+RLOpenCl6@users.noreply.github.com>
2021-10-07 20:02:37 +0200

    Add files via upload

---
+++ Sauvegarde1.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3.8
+# -*- coding:utf-8 -*-
+# Script permettant le lancement du script de sauvegarde Linux
+# Etudiant : R. Linares Rubio
+# Date : 28 Septembre 2021
+# # Version : 2.0
+
+
+# import du module
+import os
+
+# lancez une commande
+os.system("ssh p6aic@192.168.0.50")
+#'python3 -s' <. /home/p6aic/Documents/Sauvegarde/TestSauvegarde.py")   
